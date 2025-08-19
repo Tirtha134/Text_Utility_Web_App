cc53ace8da334d8626568031871751b719388095
Tirtha Samanta <samantatirtha904@gmail.com>
2025-08-20 00:23:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Text_Utility_Web_App/', // your repo name
+  base: '/Text_Utility_Web_App/Home/', // base path includes /Home/
   plugins: [react()],
 });
